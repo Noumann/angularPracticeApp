cc60025153ebd7ae1076c52e9b2aabbc6eb864eb
Noumann <32297685+Noumann@users.noreply.github.com>
2018-02-28 22:12:43 +0000

    news

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor() { }
+  constructor(private http:Http) { }
+
+  getNews():Observable<any>{
+    return this.http.get("https://jsonblob.com/api/jsonblob/7c336b8e-1cd0-11e8-8434-c571b980c55b")
+    .map(obs => obs.json());
+  }
 
 }
